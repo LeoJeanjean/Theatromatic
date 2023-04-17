075f812ed2ccc4d01ba500b9a524911faa400524
LeoJJ <leojj.pro@gmail.com>
2023-04-17 14:39:34 +0200

    deuxième commit

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -10,8 +10,16 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/personnages',
       name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/scene',
+      name: 'scene',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class='buttons'>
+    <RouterLink class='button' to="/personnages">Personnages</RouterLink>
+    <RouterLink class='button' to="/scene">Lancer une pièce</RouterLink>
+  </nav>
+</template>
+
+<style>
+.buttons{
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
