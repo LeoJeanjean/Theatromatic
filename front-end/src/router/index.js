4731252053e39531a8780a3ceb4836583c3ed6c2
Mohamed <mohameddrame907@gmail.com>
2023-04-24 10:38:14 +0200

    feat/-grid created

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -46,6 +46,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ScenarioView.vue')
+    },
+    {
+      path: '/scenegrid',
+      name: 'scenegrid',
+      component: () => import('../components/FinalSceneGrid.vue')
     }
   ]
 })
