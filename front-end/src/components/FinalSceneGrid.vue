90d9828454f2a38bbc54b21afe0af8d6889091d3
Mohamed <mohameddrame907@gmail.com>
2023-04-27 19:41:49 +0200

    feat/ the character can move

--- front-end/src/components/FinalSceneGrid.vue
+++ front-end/src/components/FinalSceneGrid.vue
@@ -39,7 +39,7 @@ export default {
             scenario: `
                [{"s":{"magasin d'informatique":[2,3],"Titouan":[0,0],"Robin":[0,7]}},{"b":"Titouan et Robin ont décidé de se rendre dans un magasin d'informatique pour acheter un nouvel ordinateur. Ils se sont tous les deux levés tôt ce matin pour faire leurs achats."},{"a":"En entrant dans le magasin, Titouan est immédiatement fasciné par les lumières vives et les écrans d'ordinateurs. Robin, quant à lui, s'est dirigé directement vers l'un des employés du magasin pour demander de l'aide.","p":[0,7,"Robin"]},{"d":"Bonjour, pouvez-vous nous aider à trouver le meilleur ordinateur pour nos besoins?","t":"Robin","p":[0,7,"Robin"]},{"d":"Bien sûr, quels sont vos besoins en matière d'ordinateur?","t":"Employé du magasin","p":[2,1,"Employé du magasin"]},{"d":"Nous cherchons un ordinateur qui est rapide et efficace pour travailler sur des projets de développement informatique","t":"Robin","p":[0,7,"Robin"]},{"d":"Je suis surtout intéressé par les ordinateurs qui ont un design cool et qui peuvent jouer à des jeux vidéo","t":"Titouan","p":[0,0,"Titouan"]},{"d":"Très bien, nous avons quelques modèles que vous pourriez aimer. Suivez-moi","t":"Employé du magasin","p":[2,1,"Employé du magasin"]},{"a":"L'employé du magasin les amène dans une section de l'établissement où des ordinateurs de jeu haut de gamme sont exposés sur des étagères en verre. Titouan est immédiatement attiré par les lumières vives et les effets de couleur sur les ordinateurs","p":[1,1,"Titouan"]},{"d":"Regarde ça! Cet ordinateur a un processeur super rapide!","t":"Robin","p":[1,4,"Robin"]},{"a":"Titouan s'approche pour voir l'ordinateur que Robin a trouvé.","p":[1,3,"Titouan"]},{"d":"Qu'en penses-tu, Titouan? C'est ce que tu recherches?","t":"Robin","p":[1,4,"Robin"]},{"d":"Je ne sais pas...","t":"Titouan","p":[1,3,"Titouan"]},{"a":"Robin explique les spécifications techniques de l'ordinateur à Titouan.","p":[1,4,"Robin"]},{"d":"Je le veux! C'est parfait!","t":"Titouan","p":[1,3,"Titouan"]},{"a":"Robin se dirige vers la caisse pour acheter l'ordinateur tandis que Titouan reste en arrière pour regarder les autres ordinateurs.","p":[0,7,"Robin"]},{"a":"Titouan passe devant un écran d'ordinateur avec un jeu vidéo à l'affichage et décide de l'essayer.","p":[1,1,"Titouan"]},{"a":"Il commence à jouer et est complètement absorbé par le jeu, oubliant complètement le temps qui passe.","p":[1,1,"Titouan"]},{"d":"Bonjour monsieur, le magasin ferme dans 10 minutes, il va falloir quitter le magasin.","t":"Employé du magasin","p":[1,1,"Employé du magasin"]},{"d":"Oh désolé, j'ai perdu la notion du temps. Je vais rejoindre mon ami à la caisse","t":"Titouan","p":[1,1,"Titouan"]},{"a":"Titouan se dirige vers la caisse et rejoint Robin qui vient juste de terminer l'achat.","p":[0,7,"Robin",1,3,"Titouan"]},{"d":"Alors Titouan, as-tu trouvé quelque chose?","t":"Robin","p":[0,7,"Robin"]},{"d":"Oui, j'ai trouvé cet ordinateur de jeu super cool!","t":"Titouan","p":[1,3,"Titouan"]},{"d":"Génial, j'ai hâte de voir à quoi ça ressemble chez nous","t":"Robin","p":[0,7,"Robin"]},{"a":"Les deux amis sortent du magasin et se dirigent vers leur voiture pour rentrer chez eux avec leur nouvel ordinateur.","p":[0,7,"Robin",1,3,"Titouan"]},{"E":"Ils sont tous les deux heureux de leur nouvel achat et sont impatients de l'utiliser pour leurs projets et leurs jeux vidéo."}]
             `,
-            scenarioGrid: `[{"s": {"arène": [2, 4], "Bob": [0, 1], "Jean": [4, 6]}}, {"b": "Bob et Jean se sont croisés dans une arène de combat."}, {"d": "Salut l'ami, tu as l'air d'avoir besoin d'un coup de main avec ton armure", "t": "Bob", "p": [0, 1, "Bob"]}, {"d": "Oh, merci beaucoup !", "t": "Jean", "p": [4, 6, "Jean"]}, {"a": "Bob aide Jean à réparer son armure et les deux commencent à discuter de leur passion pour la mécanique.", "p": [2, 2, null]}, {"d": "Je n'ai jamais vu un aussi bon travail d'ingénierie sur une armure, Bob. Tu es vraiment doué dans ce domaine.", "t": "Jean", "p": [4, 6, "Jean"]}, {"d": "Merci, je suis juste un bricoleur passionné.", "t": "Bob", "p": [0, 1, "Bob"]}, {"a": "Soudain, un lion s'échappe de sa cage et se dirige droit vers eux.", "p": [3, 4, null]}, {"d": "Je suis sûr que nous pouvons trouver un moyen de sortir de cette situation ensemble.", "t": "Bob", "p": [0, 1, "Bob"]}, {"d": "D'accord, je suis prêt.", "t": "Jean", "p": [4, 6, "Jean"]}, {"a": "Bob utilise son savoir-faire pour créer une arme à partir de matériaux trouvés dans l'arène, tandis que Jean l'utilise pour distraire le lion.", "p": [2, 2, null]}, {"d": "Attrape !", "t": "Jean", "p": [4, 6, "Jean"]}, {"a": "Jean lance une pierre sur le lion pour attirer son attention.", "p": [4, 6, "Jean"]}, {"a": "Bob lance l'arme improvisée vers le lion, atteignant sa cible avec précision et permettant à Jean de fuir.", "p": [0, 1, "Bob"]}, {"d": "Hé, tu as réussi ! Nous avons survécu grâce à toi, Bob.", "t": "Jean", "p": [4, 6, "Jean"]}, {"d": "C'était une équipe formidable. Nous devrions travailler ensemble à l'avenir.", "t": "Bob", "p": [0, 1, "Bob"]}, {"E": "Les deux se séparent, sachant qu'ils ont fait un ami pour la vie."}]`,
+            scenarioGrid: `[{"s": {"arène": [2, 4], "Bob": [0, 1], "Jean": [4, 6]}}, {"b": "Bob et Jean se sont croisés dans une arène de combat."}, {"d": "Salut l'ami, tu as l'air d'avoir besoin d'un coup de main avec ton armure", "t": "Bob", "p": [0, 1, "Bob"]}, {"d": "Oh, merci beaucoup !", "t": "Jean", "p": [0, 6, "Jean"]}, {"a": "Bob aide Jean à réparer son armure et les deux commencent à discuter de leur passion pour la mécanique.", "p": [2, 2, "Bob"]}, {"d": "Je n'ai jamais vu un aussi bon travail d'ingénierie sur une armure, Bob. Tu es vraiment doué dans ce domaine.", "t": "Jean", "p": [4, 6, "Jean"]}, {"d": "Merci, je suis juste un bricoleur passionné.", "t": "Bob", "p": [0, 1, "Bob"]}, {"a": "Soudain, un lion s'échappe de sa cage et se dirige droit vers eux.", "p": [3, 4, null]}, {"d": "Je suis sûr que nous pouvons trouver un moyen de sortir de cette situation ensemble.", "t": "Bob", "p": [0, 1, "Bob"]}, {"d": "D'accord, je suis prêt.", "t": "Jean", "p": [4, 6, "Jean"]}, {"a": "Bob utilise son savoir-faire pour créer une arme à partir de matériaux trouvés dans l'arène, tandis que Jean l'utilise pour distraire le lion.", "p": [2, 2, null]}, {"d": "Attrape !", "t": "Jean", "p": [4, 6, "Jean"]}, {"a": "Jean lance une pierre sur le lion pour attirer son attention.", "p": [4, 6, "Jean"]}, {"a": "Bob lance l'arme improvisée vers le lion, atteignant sa cible avec précision et permettant à Jean de fuir.", "p": [0, 1, "Bob"]}, {"d": "Hé, tu as réussi ! Nous avons survécu grâce à toi, Bob.", "t": "Jean", "p": [4, 6, "Jean"]}, {"d": "C'était une équipe formidable. Nous devrions travailler ensemble à l'avenir.", "t": "Bob", "p": [0, 1, "Bob"]}, {"E": "Les deux se séparent, sachant qu'ils ont fait un ami pour la vie."}]`,
             imgs: `[{"url" : "https://cdn.pixabay.com/photo/2014/07/31/21/32/bullfight-406865_1280.jpg", "nomPersoOuItem": "arène"},{"url" : "https://cdn.pixabay.com/photo/2017/07/20/18/34/plumber-2523231_1280.png", "nomPersoOuItem": "Bob"},{"url" : "https://cdn.pixabay.com/photo/2017/06/19/06/56/arrow-2418321_1280.jpg", "nomPersoOuItem": "Jean"}]`,
             api: 'https://pixabay.com/api/?key=35717457-65a4b6adba7729f12e69b314c&safesearch=true&image_type=illustration&image_type=vector&per_page=10',
             selectedImage: "",
@@ -139,6 +139,7 @@ export default {
         // }
 
 
+        characterText.innerHTML = ""
         charcterSpeech.innerHTML = parsedScenario[1]["b"];
 
         await sleep(2000)
@@ -147,29 +148,33 @@ export default {
         for (let i = 2; i < parsedScenario.length - 1; i++) {
 
           const scenarioLine = parsedScenario[i];
+
+          if (scenarioLine["p"]) {
+            console.log("¨¨¨¨");
+            console.log(scenarioLine["p"]);
+          }
           if (scenarioLine["d"]) {
             console.log(scenarioLine["d"]);
             if (scenarioLine["p"]) {
+                
               let getImgCell = document.querySelector('.c' + scenarioLine["p"][0] + "r" + scenarioLine["p"][1] + "img")
-              getImgCell.setAttribute("src", "https://cdn.pixabay.com/photo/2013/07/13/11/43/boxing-158519_1280.png")
+              this.getRightImage(scenarioLine["p"][2],getImgCell)
+              
             }
             characterText.innerHTML = scenarioLine["t"];
             charcterSpeech.innerHTML = scenarioLine["d"];
             await sleep(2000)
-
           } else {
             if (scenarioLine["p"]) {
               let getImgCell = document.querySelector('.c' + scenarioLine["p"][0] + "r" + scenarioLine["p"][1] + "img")
-              getImgCell.setAttribute("src", "https://cdn.pixabay.com/photo/2013/07/13/11/43/boxing-158519_1280.png")
+              this.getRightImage(scenarioLine["p"][2],getImgCell)
             }
             characterText.innerHTML = "";
             charcterSpeech.innerHTML = scenarioLine["a"];
             await sleep(2000)
           }
-
-
         }
-
+        characterText.innerHTML = ""
         charcterSpeech.innerHTML = parsedScenario[parsedScenario.length - 1]["E"];
 
 
@@ -191,10 +196,30 @@ export default {
       },
       getRandomInt(max) {
         return Math.floor(Math.random() * max);
-      }
-    },
-    mounted() {
-        
+      },
+      getRightImage(nameCharacterOrItem, getImgCell) {
+
+        var images = document.getElementsByTagName('img');
+
+        for (const key in JSON.parse(this.imgs)) {
+                if(nameCharacterOrItem == JSON.parse(this.imgs)[key]["nomPersoOuItem"]) {
+
+                    for (let i = 0; i < images.length; i++) {
+
+                        if (images[i].src == JSON.parse(this.imgs)[key]["url"] && images[i].className !== "c"+getImgCell[1]+"r"+getImgCell[3]) {
+                            images[i].setAttribute('src',"");
+                        }
+                    }
+
+
+
+                    getImgCell.setAttribute("src", JSON.parse(this.imgs)[key]["url"] )
+                    return JSON.parse(this.imgs)[key]["url"]
+                }                
+            }
+        return null
+
+    }
     }
 }
 </script>
